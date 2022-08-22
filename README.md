6b895a620cbd12773c76fe1462aced312b70b989
SHAHAN AHMAD <shahan.ahmad@hotmail.com>
2022-08-23 00:13:54 +1000

    project

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# LandingPage
+# Landing page.
+
+## DomManipulation.
+
+<p>This project is very Challenging i learn lot of fundamental tools and skills from this project
+Youtube channel is big help to learn and how to imeplement knowledge in real project which i have been learning through this course. I am very greatfull to Dcode channel and traverse media had lot of great video  about DOM manipulation i took lot of ideas from there channel  and help from mdn web docs and w3school reading material were very helpful<p>
+
+## File Structure
+
+- css <br>
+  styles.css
+- index.html
+- js<br>
+  app.js
+
+- README.md
+
+
+* Navigation is built dynamically as an unordered list. Start with empty ul and dynamically build navigation createElement appendChild, and innerHTML.
+
+
+```javascript
+createNavMenu();
+```
+
+
+- Styling has been added for active sections and links .
+
+```javascript
+IntersectionObserver()
+```
+
+- Scroll to section on link click using scrollIntoView
+
+```javascript
+section.scrollIntoView({
+      behavior: "smooth",
+      block: 'start'    
+     });
+```
+
+* Font-awesome icon used to take totop by clicking
+
+        <html>
+          <div id="TopPage">
+       <i class="fa fa-arrow-up"aria-hidden="true"></i>
+          </div>
+     <html>
+          
+          
+   <p>Thankyou udacity for indepth tutorial and to the details lesson. It has been great learning experience. <p>       
+
+
+
---
+++ js/app.js
@@ -0,0 +1,113 @@
+/*
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ */
+/**
+ * Define Global Variables
+ * 
+*/
+const navList = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+
+// build the nav
+
+const createNavMenu = () => {
+  // make ul list items for each sections by iteration//
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="#${section.id}" data-nav="${section.id}" class="menu__link" >${section.dataset.nav}</a>`;
+    navList.appendChild(li);
+    //console.log(navList)
+  });
+};
+createNavMenu();
+
+
+// Adding class 'active' to section when near top of viewport//
+//Adding and removing active to section links and view port//
+const navBarLink = document.querySelectorAll(".navbar__menu a");
+
+const options = {
+  threshold: '0.5',
+ 
+}
+
+const observer = new IntersectionObserver((entries) =>{
+  
+ entries.forEach((e)=>{
+  //console.log(e.target.id)
+  e.target.classList.remove("your-active-class")
+   if(e.isIntersecting  ){
+    e.target.classList.add("your-active-class")
+    navBarLink.forEach((link)=>{
+      link.classList.remove("active-link")
+      if(e.target.id === link.dataset.nav ){
+      link.classList.add("active-link")
+     }
+    
+     })
+   }
+ })
+}, options);
+
+sections.forEach((section)=>{
+  observer.observe(section)
+})
+
+
+// Scroll to section on link click
+
+navBarLink.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    e.preventDefault();
+    const sectionId = e.target.attributes.href.value;
+    //console.log(sectionId)
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: 'start'    
+     });
+
+   
+  });
+});
+
+//Header hide out after 3 sec while not scrolling
+const header = document.querySelector(".page__header");
+
+let checkScroll ;
+let prevScrollY = window.pageYOffset;
+window.addEventListener('scroll', ()=>{
+  clearTimeout(checkScroll)
+  if(prevScrollY < window.pageYOffset){
+    checkScroll = setTimeout(()=>{
+      header.style.opacity = '0';
+    },3000)
+    }else{
+      header.style.opacity = '1';
+      }
+    prevScrollY = window.pageYOffset
+})
+
+// scroll to top icon
+
+const TopPage = document.querySelector("#TopPage");
+// toggle active class to hide and show icon after 600 px
+window.addEventListener('scroll', ()=>{
+  TopPage.classList.toggle('active', window.scrollY > 600)
+})
+
+// scroll to top by clicking icon on bottom page
+TopPage.addEventListener("click", () => {
+  window.scrollTo({ top: 0,
+                    left: 0,
+                  behavior: "smooth" });    
+});
+
